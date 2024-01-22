fa393058a811838005ea6b424bcbff21d040f7f6
kindkains <kindkains@gmx.de>
2024-01-22 11:35:31 +0100

    Verbessert

--- sonstiges/ipRaumScan.sh
+++ sonstiges/ipRaumScan.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-echo -e "IP-Adresse\tHostname" > /home/sandro/skripte/ergebnis
-echo -e "==========\t========" >> /home/sandro/skripte/ergebnis
-echo -e "" >> /home/sandro/skripte/ergebnis
+output=ergebnis
+
+echo -e "IP-Adresse\tHostname" > $output
+echo -e "==========\t========" >> $output
+echo -e "" >> $output
 
 x=1
-y=$x
-ip="192.168.0."
-#ip="172.24.30."
-#ip="192.168.199."
-#ip="192.168.201."
+clear
+echo "Welcher Netzbereich soll gescant werden?"
+read -p "Bsp. für 192.168.0.0/24 eine 0 eingeben, eine 1 für 192.168.1.0/24):  " y
+ip="192.168.$y.$x"
 
 while [ $x -lt 256 ];do
-	if [ $(nslookup $ip$x | grep "name" &>/dev/null; echo $?) = 0 ];then
-		host=$(nslookup $ip$x | grep "name" | awk -F "name = " '{print $2}')
-		echo -e "$ip$x\t$host" >> /home/sandro/skripte/ergebnis
+	if [ $(nslookup $ip | grep "name" &>/dev/null; echo $?) = 0 ];then
+		host=$(nslookup $ip | grep "name" | awk -F "name = " '{print $2}')
+		echo -e "{$ip}\t${host}" >> $output
+		echo "Scanne $ip | Treffer!"
 		((x=$x+1))
+		ip="192.168.$y.$x"
 	else
+		echo "Scanne $ip | Keine Antwort!"
 		((x=$x+1))
+		ip="192.168.$y.$x"
 	fi
 done 
 
 clear
-echo "Folgende Hosts wurden im IP-Raum von $ip$y bis $ip$x gefunden:"
+echo "Folgende Hosts wurden im IP-Raum von 192.168.${y}.1 bis ${ip} gefunden:"
 echo -e "\n"
-cat /home/sandro/skripte/ergebnis
+cat $output
--- sonstiges/ipRaumScan2.sh
+++ sonstiges/ipRaumScan2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-echo -e "IP-Adresse\tHostname" > /home/sandro/skripte/ergebnis
-echo -e "==========\t========" >> /home/sandro/skripte/ergebnis
-echo -e "" >> /home/sandro/skripte/ergebnis
+output=ergebnis
+
+echo -e "IP-Adresse\tHostname" > $output
+echo -e "==========\t========" >> $output
+echo -e "" >> $output
 
 x=1
 y=$x
 ip1="172.24.30."
-ip2="192.168.199."
+ip2="192.168.99."
 ip3="192.168.201."
 
 while [ $x -lt 256 ];do
 	if [ $(nslookup $ip1$x | grep "name" &>/dev/null; echo $?) = 0 ];then
 		host=$(nslookup $ip1$x | grep "name" | awk -F "name = " '{print $2}')
-		echo -e "$ip1$x\t$host" >> /home/sandro/skripte/ergebnis
+		echo -e "$ip1$x\t$host" >> $output
 		((x=$x+1))
 	else
 		((x=$x+1))
@@ -23,8 +25,8 @@ done
 clear
 echo "Folgende Hosts wurden im IP-Raum von 172.24.30.$y bis 172.24.30.$x gefunden:"
 echo -e "\n"
-echo "" >> /home/sandro/skripte/ergebnis
-cat /home/sandro/skripte/ergebnis
+echo "" >> $output
+cat $output
 
 x=1
 y=$x
@@ -32,7 +34,7 @@ y=$x
 while [ $x -lt 256 ];do
 	if [ $(nslookup $ip2$x | grep "name" &>/dev/null; echo $?) = 0 ];then
 		host=$(nslookup $ip2$x | grep "name" | awk -F "name = " '{print $2}')
-		echo -e "$ip2$x\t$host" >> /home/sandro/skripte/ergebnis
+		echo -e "$ip2$x\t$host" >> $output
 		((x=$x+1))
 	else
 		((x=$x+1))
@@ -41,8 +43,8 @@ done
 
 echo "Folgende Hosts wurden im IP-Raum von 192.168.199.$y bis 192.168.199.$x gefunden:"
 echo -e "\n"
-echo "" >> /home/sandro/skripte/ergebnis
-cat /home/sandro/skripte/ergebnis
+echo "" >> $output
+cat $output
 
 x=1
 y=$x
@@ -50,7 +52,7 @@ y=$x
 while [ $x -lt 256 ];do
 	if [ $(nslookup $ip3$x | grep "name" &>/dev/null; echo $?) = 0 ];then
 		host=$(nslookup $ip3$x | grep "name" | awk -F "name = " '{print $2}')
-		echo -e "$ip3$x\t$host" >> /home/sandro/skripte/ergebnis
+		echo -e "$ip3$x\t$host" >> $output
 		((x=$x+1))
 	else
 		((x=$x+1))
@@ -60,4 +62,4 @@ done
 clear
 echo "Folgende Hosts wurden im IP-Raum von 192.168.201.$y bis 192.168.201.$x gefunden:"
 echo -e "\n"
-cat /home/sandro/skripte/ergebnis
+cat $output
